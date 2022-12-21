c5f9aba43528fb056f44c88f4f0b242ff8d47a50
sapock <49747084+sapock@users.noreply.github.com>
2022-12-21 17:25:24 +0900

    commit

---
+++ inventory/src/test/java/labshopmonolithsola/main/TestMain.java
@@ -0,0 +1,13 @@
+package labshopmonolithsola.main;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin={"pretty","html:target/cucumber"},
+                features = "src/test/resources/features",
+                extraGlue="labshopmonolithsola/common")
+public class TestMain {
+    
+}
---
+++ monolith/src/main/java/labshopmonolithsola/domain/OrderRepository.java
@@ -0,0 +1,10 @@
+package labshopmonolithsola.domain;
+
+import labshopmonolithsola.domain.*;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel="orders", path="orders")
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
+
+}
