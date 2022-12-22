a60839f47c953a4169319c57adcef95ee3dcb89d
sapock <49747084+sapock@users.noreply.github.com>
2022-12-22 10:45:02 +0900

    order.java 수정

---
+++ monolith/src/main/java/labshopmonolithsola/external/DecreaseStockCommand.java
@@ -0,0 +1,12 @@
+package labshopmonolithsola.external;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class DecreaseStockCommand {
+
+    private Integer qty;
+}
---
+++ monolith/src/test/java/labshopmonolithsola/common/CucumberSpingConfiguration.java
@@ -0,0 +1,12 @@
+package labshopmonolithsola.common;
+
+
+import labshopmonolithsola.MonolithApplication;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest(classes = { MonolithApplication.class })
+public class CucumberSpingConfiguration {
+    
+}
